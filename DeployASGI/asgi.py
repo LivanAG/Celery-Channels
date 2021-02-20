661304673759935722f1cef978dc8f913b9945a7
Livan <livanar00@gmail.com>
2021-02-20 16:15:47 -0500

    Modificando asgi con djangosetup

--- DeployASGI/asgi.py
+++ DeployASGI/asgi.py
@@ -13,9 +13,10 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter,URLRouter
 from channels.auth import AuthMiddlewareStack
 from .routing import *
+import django 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DeployASGI.settings')
-
+django.setup()
 application = ProtocolTypeRouter({
     "http":get_asgi_application(),
 
