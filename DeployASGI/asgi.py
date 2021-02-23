a20dc3cc2b9648d6771bcc387b701d5cc4763e15
Livan <livanar00@gmail.com>
2021-02-23 14:18:31 -0500

    vamos

--- DeployASGI/asgi.py
+++ DeployASGI/asgi.py
@@ -6,7 +6,7 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
-
+'''
 import os
 
 from django.core.asgi import get_asgi_application
@@ -23,6 +23,19 @@ application = ProtocolTypeRouter({
 
     "websocket":AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
 })
+'''
 
 
+import os
 
+from django.core.asgi import get_asgi_application
+from .routing import *
+from channels.routing import ProtocolTypeRouter,URLRouter
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DeployASGI.settings")
+
+application = ProtocolTypeRouter({
+    "http":get_asgi_application(),
+
+    "websocket":URLRouter(websocket_urlpatterns),
+})
